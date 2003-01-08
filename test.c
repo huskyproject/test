07bec21cdd44c776db9fdaa6d1474a594695ae8e
Andy U. Luk'yanov <andr_lukyanov@users.sourceforge.net>
2003-01-08 03:04:08 +0000

    Testing ssh connection

--- test.c
+++ test.c
@@ -6,3 +6,5 @@ test2 for Ivan Fedorov
 test3 for Ivan Fedorov
 /* test from Andy Luk'yanov */
 This is test line from me.
+/* Second test from Andy Luk'yanov */
+This is second test line from me. Add line in ChangeLog
