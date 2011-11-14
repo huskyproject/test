13a0dccdceafe409eb5e19b8ff731fa214a2b74e
Stas Degteff <stas_degteff@users.sourceforge.net>
2011-11-14 20:52:04 +0000

    Little endian test program

--- test.c
+++ test.c
@@ -11,9 +11,13 @@ Test  endian (little endian or big endian)
 #include <string.h>
 
 main(){
- unsigned int ival=0x04030201;
+ unsigned int ival=0x34333231;
  char sval[5]="";
  memcpy(sval,&ival,4);
- printf("Endian test: integer %08X(hex), bytes: 1:%c 2:%c 3:%c 4:%c\n", ival, sval[0]+'0', sval[1]+'0', sval[2]+'0', sval[3]+'0' );
+ printf("Little Endian test. Result \"1234\" should be on little-endian platform\n"
+        "Integer in hex: %08X(hex), bytes: 1:%c 2:%c 3:%c 4:%c\n", ival, sval[0], sval[1], sval[2], sval[3] );
+ printf("Binary representation of 32-bit integer in IO operation: \"");
+ fwrite(&ival, sizeof(ival), 1, stdout);
+ printf("\"\n");
  return (int)ival;
 }
