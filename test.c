976dcef90f99afce1e12a1a4262d0bf05cfa6d48
Stas Degteff <stas_degteff@users.sourceforge.net>
2011-11-14 20:29:07 +0000

    bigendian/littleendian test

--- test.c
+++ test.c
@@ -1,10 +1,19 @@
 /*
 $Id$
-
-This is a test file
-
-Testing keywords:
    $Revision$
    $Date$
    $Author$
+
+Test  endian (little endian or big endian)
 */
+
+#include <stdio.h>
+#include <string.h>
+
+main(){
+ unsigned int ival=0x04030201;
+ char sval[5]="";
+ memcpy(sval,&ival,4);
+ printf("Endian test: integer %08X(hex), bytes: 1:%c 2:%c 3:%c 4:%c\n", ival, sval[0]+'0', sval[1]+'0', sval[2]+'0', sval[3]+'0' );
+ return (int)ival;
+}
